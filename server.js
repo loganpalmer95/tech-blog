97868754d99ead150778631b8132d74c1f234ffc
Logan Palmer <loganpalmer95@icloud.com>
2022-03-29 21:19:50 -0400

    syntax error

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({
-    helpers
-});
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
 
 const sess = {
     secret: "secret secret",
@@ -22,22 +22,27 @@ const sess = {
     })
 };
 
-const app = express();
+app.use(session(sess));
+
+const hbs = exphbs.create({
+    helpers
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 //sequelize.sync();
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
